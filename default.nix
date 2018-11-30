3b552406934ab9040d16259cd87ad4bea8345cd4
Brian McKenna <bmckenna@atlassian.com>
2018-11-30 22:37:47 +0530

    Remove -hide-package etlas-cabal for dhall-to-etlas

--- default.nix
+++ default.nix
@@ -10,11 +10,6 @@
         sed -iE 's/tagged >=.*/tagged/g' semigroupoids.cabal
       '';
     });
-    dhall-to-etlas = pkgs.haskell.lib.overrideCabal super.dhall-to-etlas (drv: {
-      preCompileBuildDriver = ''
-        setupCompileFlags="$setupCompileFlags -hide-package etlas-cabal"
-      '';
-    });
     tasty = pkgs.haskell.lib.doJailbreak super.tasty;
   } // overrides self super;
 }
